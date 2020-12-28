45859b6846d03fb9328661a684d0ded5df431738
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-28 18:51:22 +0530

    Camera Save

--- App.js
+++ App.js
@@ -1,57 +1,57 @@
-import React, { PureComponent } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, { Component } from 'react'
+import { StyleSheet, View, Alert ,TouchableOpacity,Text,ToastAndroid,CameraRoll} from 'react-native'
+import { RNCamera } from 'react-native-camera'
+class App extends Component {
 
-class ExampleApp extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
         <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
           style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
+          ref={ref => {
+            this.camera = ref
           }}
         />
         <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
           <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}> SNAP </Text>
+            <Text style={{ fontSize: 14 }}> Take Picture  </Text>
           </TouchableOpacity>
         </View>
       </View>
-    );
+    )
   }
 
-  takePicture = async () => {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
-  };
+takePicture = async () => {
+  if (this.camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await this.camera.takePictureAsync(options).then(data => {
+      ToastAndroid.show(data.uri, ToastAndroid.SHORT);
+      CameraRoll.saveToCameraRoll(data.uri);
+      console.log(data);
+    //console.log(data.uri);
+    //this.saveImage(data.uri)
+  })
 }
+};
+saveImage = async filePath => {
+  try {
+    // set new image name and filepath
+    const newImageName = `${moment().format('DDMMYY_HHmmSSS')}.jpg`;
+    const newFilepath = `${dirPicutures}/${newImageName}`;
+    // move and save image to new filepath
+    const imageMoved = await moveAttachment(filePath, newFilepath);
+    console.log('image moved', imageMoved);
+  } catch (error) {
+    console.log('Catch'+error);
+  }
+};
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'black',
+    backgroundColor: 'black'
   },
   preview: {
     flex: 1,
@@ -67,6 +67,6 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     margin: 20,
   },
-});
+})
 
-export default ExampleApp;
+export default App
