f8570445992e0bbf9658b5773d6dfc2da9d133ab
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-29 19:34:42 +0530

    Video Recording

--- App.js
+++ App.js
@@ -4,102 +4,26 @@ import {
   Text,
   View,
   TouchableWithoutFeedback,
+  TouchableOpacity,
 } from 'react-native';
 import Video from 'react-native-video';
-
+import VideoRecorder from 'react-native-beautiful-video-recorder';
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      paused: true,
-      pausedText: 'Play',
-      hideControls: false,
-    };
-    this.video = Video;
+  start = () => {
+  // 30 seconds
+    this.videoRecorder.open({ maxLength: 30 },(data) => {
+      console.log('captured data', data);
+    });
   }
-
-  onPressBtnPlay() {
-    var pausedText = '';
-    if(!this.state.paused){
-      pausedText = 'Play';
-    }
-    else {
-      pausedText = 'Pause';
-    } 
-    this.setState({ paused: !this.state.paused, pausedText: pausedText });
-  }
-  onPressVideo() {
-    if(this.state.hideControls){
-      this.setState({hideControls: false});
-      this.timeoutHandle = setTimeout(()=>{
-        this.setState({hideControls: true});
-      }, 8000);
-    }
-  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableWithoutFeedback
-          style={styles.fullScreen}
-          onPress={() => this.onPressVideo()}>
-          <Video
-            ref={(ref: Video) => { this.video = ref }}
-            source={{ uri: 'https://rawgit.com/uit2712/Mp3Container/master/tom_and_jerry_31.mp4' }} 
-           // source={require('./videos/mala.mp4')}
-            style={styles.fullScreen}
-            paused={this.state.paused}
-           
-          />
-        </TouchableWithoutFeedback>
-        {
-          !this.state.hideControls ?
-          (
-            <View style={styles.controls}>
-              <View style={styles.generalControls}>
-                <View style={styles.playControl}>
-                  <Text onPress={() => this.onPressBtnPlay()}>{this.state.pausedText}</Text>
-                </View>         
-              </View>
-            </View>
-          ) : (null)
-        }
+      <View>
+        <TouchableOpacity onPress={this.start}>
+          <Text>Start</Text>
+        </TouchableOpacity>
+        <VideoRecorder ref={(ref) => { this.videoRecorder = ref; }} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  fullScreen: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  playButton: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    right: 20,
-  },
-  controls: {
-    backgroundColor: 'white',
-    opacity: 0.7,
-    borderRadius: 5,
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    right: 20,
-  },
-  playControl: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-});
