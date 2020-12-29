0af31a680538413966dbb775a457bcc6a56f427f
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-29 15:53:39 +0530

    Video Player

--- App.js
+++ App.js
@@ -1,72 +1,105 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Alert ,TouchableOpacity,Text,ToastAndroid,CameraRoll} from 'react-native'
-import { RNCamera } from 'react-native-camera'
-class App extends Component {
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import Video from 'react-native-video';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paused: true,
+      pausedText: 'Play',
+      hideControls: false,
+    };
+    this.video = Video;
+  }
+
+  onPressBtnPlay() {
+    var pausedText = '';
+    if(!this.state.paused){
+      pausedText = 'Play';
+    }
+    else {
+      pausedText = 'Pause';
+    } 
+    this.setState({ paused: !this.state.paused, pausedText: pausedText });
+  }
+  onPressVideo() {
+    if(this.state.hideControls){
+      this.setState({hideControls: false});
+      this.timeoutHandle = setTimeout(()=>{
+        this.setState({hideControls: true});
+      }, 8000);
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <RNCamera
-          style={styles.preview}
-          ref={ref => {
-            this.camera = ref
-          }}
-        />
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}> Take Picture  </Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableWithoutFeedback
+          style={styles.fullScreen}
+          onPress={() => this.onPressVideo()}>
+          <Video
+            ref={(ref: Video) => { this.video = ref }}
+            source={{ uri: 'https://rawgit.com/uit2712/Mp3Container/master/tom_and_jerry_31.mp4' }} 
+           // source={require('./videos/mala.mp4')}
+            style={styles.fullScreen}
+            paused={this.state.paused}
+           
+          />
+        </TouchableWithoutFeedback>
+        {
+          !this.state.hideControls ?
+          (
+            <View style={styles.controls}>
+              <View style={styles.generalControls}>
+                <View style={styles.playControl}>
+                  <Text onPress={() => this.onPressBtnPlay()}>{this.state.pausedText}</Text>
+                </View>         
+              </View>
+            </View>
+          ) : (null)
+        }
       </View>
-    )
+    );
   }
-
-takePicture = async () => {
-  if (this.camera) {
-    const options = { quality: 0.5, base64: true };
-    const data = await this.camera.takePictureAsync(options).then(data => {
-      ToastAndroid.show(data.uri, ToastAndroid.SHORT);
-      CameraRoll.saveToCameraRoll(data.uri);
-      console.log(data);
-    //console.log(data.uri);
-    //this.saveImage(data.uri)
-  })
 }
-};
-saveImage = async filePath => {
-  try {
-    // set new image name and filepath
-    const newImageName = `${moment().format('DDMMYY_HHmmSSS')}.jpg`;
-    const newFilepath = `${dirPicutures}/${newImageName}`;
-    // move and save image to new filepath
-    const imageMoved = await moveAttachment(filePath, newFilepath);
-    console.log('image moved', imageMoved);
-  } catch (error) {
-    console.log('Catch'+error);
-  }
-};
 
-}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
+  fullScreen: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  playButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+  },
+  controls: {
+    backgroundColor: 'white',
+    opacity: 0.7,
     borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
   },
-})
-
-export default App
+  playControl: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
