0b7e6806c0e45212bce1c204e5b8aae1ace11d8b
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-30 17:51:09 +0530

    Animation

--- App.js
+++ App.js
@@ -1,29 +1,62 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-} from 'react-native';
-import Video from 'react-native-video';
-import VideoRecorder from 'react-native-beautiful-video-recorder';
-export default class App extends Component {
-  start = () => {
-  // 30 seconds
-    this.videoRecorder.open({ maxLength: 30 },(data) => {
-      console.log('captured data', data);
-    });
-  }
+import {View, Text, Animated, TouchableOpacity} from 'react-native';
+import React, { Component } from "react";
+
+
+ class App extends Component {
   
+  state={value:new Animated.ValueXY({x:0,y:0})};
+
+   moveBall=()=> {
+    Animated.timing(this.state.value,
+      {
+        toValue:{x:100,y:100},
+        duration:1000,
+        useNativeDriver:false,
+
+      }).start() 
+  };
+
+  bounce=()=>{
+    Animated.spring(this.state.value,{
+      toValue:{x:100,y:90},
+      bounciness:50,
+      useNativeDriver:false,
+    }).start()
+  }
+
+  reset=()=>{
+    Animated.timing(this.state.value,{
+      toValue:{x:0,y:0},
+      useNativeDriver:false,
+      duration:1000,
+    }).start()
+  }
   render() {
+    const Value=this.state.value;
     return (
-      <View>
-        <TouchableOpacity onPress={this.start}>
-          <Text>Start</Text>
+      <View style={{flex:1}}>  
+        <Animated.View style={Value.getLayout()}>
+          <View
+          style={{
+              width:100,
+              height:100,
+              borderRadius:100/2,
+              backgroundColor:'blue',
+          }}/>   
+
+        </Animated.View>
+        <TouchableOpacity onPress={this.moveBall}>
+         <Text>HELOO</Text>
         </TouchableOpacity>
-        <VideoRecorder ref={(ref) => { this.videoRecorder = ref; }} />
+        <TouchableOpacity onPress={this.bounce}>
+         <Text>Bounce</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.reset}>
+         <Text>Reset</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+export default App;
