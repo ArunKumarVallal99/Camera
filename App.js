3640971f37b704617926ae47e2496fc35e8fa806
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2021-01-06 18:46:48 +0530

    Leave Apply

--- App.js
+++ App.js
@@ -1,151 +1,90 @@
-import {View, Text, Animated, TouchableOpacity, FlatList, StyleSheet, ActivityIndicator,LayoutAnimation, Platform, UIManager} from 'react-native';
+import {View, Text, Animated, TouchableOpacity, FlatList, StyleSheet,ScrollView,Picker} from 'react-native';
 import React, { Component } from "react";
 import Icon  from 'react-native-vector-icons/MaterialIcons';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
- class App extends Component {
-  constructor(props) {
+class App extends Component {
+  constructor(props){
     super(props);
     this.state={
+      casualLeave:12,
+      sickLeave:12,
+      leaveType:'Select Leave Type',
+    };
+  }  
 
-    datas:[
-      {id:'1',title:'Veg Briyani',des:'Biryani also known as biriyani, biriani, birani or briyani, is a mixed rice dish with its origins among the Muslims of the Indian subcontinent. This dish is especially popular throughout the Indian subcontinent, as well as among the diaspora from the region. It is also prepared in other regions such as Iraqi Kurdistan.'
-      ,isEnable:false},
-      {id:'2',title:'Pizzas',des:'dakhsakhsadasd',isEnable:false,
-      pizaItems:[{key:"Chicken Dominator", value:false},{key:'Peri Peri Chicken', value:false}]},
-
-      {id:'3',title:'Drink',des:'kjjhasdgasdasj',isEnable:false},
-      {id:'4',title:'Deserets',des:'gsadfsadjgsadkusd',isEnable:false},
-
-    ],  
-  };
-  if (Platform.OS === 'android') {
-    UIManager.setLayoutAnimationEnabledExperimental(true);
-}
-}
-  isEnabled=({item,index})=>{
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-    const data=this.state.datas;
-    const target=data[index]; 
-    target.isEnable=!target.isEnable;
-    data[index]=target;
-    this.setState({datas:data})
-   // console.log(this.state.datas[index])    
-  }
-  onClick=(index)=>{
-    const temp = this.state.datas;
-    var mainIndex=0;
-    for(var i=0;i<4;i++)
-    {
-      if(temp[i].isEnable){
-         mainIndex=i;
-      }
-    }
-    console.log(mainIndex)
-    console.log(temp[mainIndex].pizaItems[index].value)
-    temp[mainIndex].pizaItems[index].value=!temp[mainIndex].pizaItems[index].value
-    this.setState({data: temp})
-  }
-  renderItems=({item,index})=>{
-      return(
-    <View style={styles.containers}>
-
-      <View style={styles.itemContainer}>
-        <TouchableOpacity onPress={()=>this.isEnabled({item,index})} style={styles.row}>
-          <Text style={styles.itemText}>{item.title}</Text>
-          <Icon name={item.isEnable? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} color='grey' />
-        </TouchableOpacity>
-      </View>
-            <View style={styles.desView}/>
-             {item.isEnable && (
-             <View style={styles.desText}>
-               <FlatList
-               data={item.pizaItems}
-               renderItem={({item, index}) => 
-               <View>
-                   <TouchableOpacity style={[styles.childRow, styles.button, item.value ? styles.btnActive : styles.btnInActive]} onPress={()=>this.onClick(index)}>
-                       <Text style={styles.desText} >{item.key}</Text>
-                       <Icon name={'check-circle'} size={24} color={ item.value ? 'green' :  'white'} style={styles.icons} />
-                   </TouchableOpacity>
-                   {/* <View style={styles.des}/> */}
-               </View>}
-               />
-          
-                 <Text>{item.des}</Text>   
-              </View>)
-            }
-    </View>
-    )
-  };
   render(){
     return(
-        <View style={styles.container}>
-          <FlatList 
-            data={this.state.datas}
-            renderItem={this.renderItems}
-            extraData={this.state}
-          />
+      <View style={styles.container}>
+        <View style={styles.insideContainer}>
+
+          <View style={styles.totalDayContainer} >
+            <Text style={styles.totalDayText}>Sick Leave: {this.state.sickLeave}</Text>
+            <Text style={styles.totalDayText}>Casual Leave: {this.state.casualLeave}</Text>
+          </View>
+
+          <View style={styles.pickerView}>
+            <Text style={styles.pickerText}>Leave Type</Text>
+            <Picker style={styles.pickers}
+            selectedValue={this.state.leaveType}
+             onValueChange={(itemValue, itemIndex) => {this.setState({leaveType:itemValue});console.log(this.state.leaveType)}}>
+            <Picker.Item label='Select Leave Type' value='default'/>
+            <Picker.Item label="Sick Leave" value="sick" />
+            <Picker.Item label="Casual Leave" value="casual"/>
+            </Picker>
+          </View>
+
+
         </View>
-      );
-    }
+      </View>
+      
+      
+    );
+  }
 }
-const styles=StyleSheet.create({
+
+const styles= StyleSheet.create({
   container:{
-    backgroundColor:'#a4c639',
-    justifyContent:'center',
-    paddingTop:100,
-    flex:1,
+   padding:20,
+   justifyContent:'center',
+   alignItems:'center',
+   alignContent:'center',
+   flex:1,
   },
-  itemContainer:{
-    flexDirection:'row',
-    padding:10,
+  insideContainer:{
+    elevation:5,
     backgroundColor:'white',
-    borderColor:'black',
-    marginBottom:5,
-    height:56,
-    paddingLeft:25,
-    paddingRight:18,
-    alignItems:'center',
+    width:350,
+    padding:20,
   },
-  row:{
+  totalDayContainer:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+    elevation:4,
+    backgroundColor:'white',
+    borderRadius:5,
+    marginBottom:10,
+    borderWidth:.5,
+  },
+  totalDayText:{
+    marginRight:20,
+    fontSize:18,
+    fontWeight:'bold',
+  },
+  pickerView:{
     flexDirection:'row',
   },
-  itemText:{
-    width:310,
-    fontSize:14,
-    fontWeight:'bold',
-    color:'grey',
+  pickerText:{
+    marginRight:20,
+    fontSize:18,
+    marginTop:10,
   },
-  childRow:{
-    flexDirection: 'row',
-    justifyContent:'space-between',
-    backgroundColor: 'grey',
-},
-  icons:{
-    paddingTop:15,
-  },
-  itemActive:{
-    fontSize: 12,
-    color: 'green',
-  },
-  itemInActive:{
-    fontSize: 12,
-    color:'black',
-  },
-  btnActive:{
-    borderColor: 'green',
-  },
-  btnInActive:{
-    borderColor: 'green',
-  },
-  desView:{
-    height:1,
-    color: 'white',
-    width:'100%'
-  },
-  desText:{
-  backgroundColor: 'grey',
-  padding:16,
+  pickers:{
+    height: 50, 
+    width: 200,
+    borderWidth:1,
+    borderColor:'blue',
   },
 });
-
 export default App;
